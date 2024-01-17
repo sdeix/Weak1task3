4d2254f6f7d110f7fa94694c3c3dcf671ed43030
sdeix <sdeix2765@gmail.com>
2024-01-17 19:44:47 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let eventBus = new Vue()
+
+Vue.component("board", {
+    template: `
+<div class="board">
+</div>
+    `,
+    data() {
+        return{
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    },
+    props:{  
+    },
+    computed: {
+    }
+});
+
+
+let app = new Vue({
+    el: "#app",
+    data: {
+    },
+    methods: {
+
+    },
+});
