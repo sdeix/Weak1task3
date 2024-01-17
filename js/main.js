b03b7dad58078cc777cf66e46e0fb93edab7fbd3
sdeix <sdeix2765@gmail.com>
2024-01-17 20:09:34 +0700

    Создана базовая архитектура

--- js/main.js
+++ js/main.js
@@ -3,6 +3,71 @@ let eventBus = new Vue()
 Vue.component("board", {
     template: `
 <div class="board">
+<ul class="columns">
+
+<li class="column">
+    <h3>Запланированные задачи</h3>  
+    <ul>
+        <li v-for="card in column1">
+            <card :column=1></card>
+        </li>
+    </ul>
+</li>
+
+<li class="column">
+    <h3>Задачи в работе</h3>
+    <ul>
+        <li v-for="card in column2">
+            <card :column=2></card>
+        </li>
+    </ul>
+</li>
+
+<li class="column">
+    <h3>Тестирование</h3> 
+    <ul>
+        <li v-for="card in column3">
+            <card :column=3></card>
+        </li>
+    </ul> 
+</li>
+
+<li class="column">
+    <h3>Выполненные задачи</h3>  
+    <ul>
+        <li v-for="card in column4">
+            <card :column=4></card>
+        </li>
+    </ul>
+</li>
+
+</ul>
+</div>
+    `,
+    data() {
+        return{
+            column1:[],
+            column2:[],
+            column3:[],
+            column4:[],
+
+            allcolumns:[],
+
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    },
+    props:{  
+    },
+    computed: {
+    }
+});
+
+Vue.component("card", {
+    template: `
+<div class="card">
 </div>
     `,
     data() {
@@ -13,7 +78,10 @@ Vue.component("board", {
     },
     mounted() {
     },
-    props:{  
+    props:{ 
+        column:{
+            type:Boolean
+        } 
     },
     computed: {
     }
