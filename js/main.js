8d8370614dd111330f714a4a3eb98332c6c1ea4a
Сергей Малюгов <gr402_maspe@lab-407-4.tomtit-tomsk.ru>
2024-01-18 09:23:37 +0700

    реализовано редактирование карточек

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ Vue.component("board", {
       </form>  
     <ul>
         <li v-for="card in column1">
-            <card @deletethis="Delete" @moveright="MoveR" :column=1 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
+            <card @deletethis="Delete" @moveright="MoveR" @edit="EditCard" :last_red="card.last_red" :column=1 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
         </li>
     </ul>
 </li>
@@ -41,7 +41,7 @@ Vue.component("board", {
     <h3>Задачи в работе</h3>
     <ul>
         <li v-for="card in column2">
-            <card @deletethis="Delete" @moveleft="MoveL" @moveright="MoveR" :column=2 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
+            <card @deletethis="Delete" @moveright="MoveR" @edit="EditCard" :last_red="card.last_red" :column=2 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
         </li>
     </ul>
 </li>
@@ -50,7 +50,7 @@ Vue.component("board", {
     <h3>Тестирование</h3> 
     <ul>
         <li v-for="card in column3">
-            <card @deletethis="Delete" @moveleft="MoveL" @moveright="MoveR" :column=3 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
+            <card @deletethis="Delete" @moveleft="MoveL" @edit="EditCard" :last_red="card.last_red" @moveright="MoveR" :column=3 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
         </li>
     </ul> 
 </li>
@@ -59,7 +59,7 @@ Vue.component("board", {
     <h3>Выполненные задачи</h3>  
     <ul>
         <li v-for="card in column4">
-            <card @deletethis="Delete" :column=4 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
+            <card @deletethis="Delete" :last_red="card.last_red" :column=4 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime"></card>
         </li>
     </ul>
 </li>
@@ -78,6 +78,8 @@ Vue.component("board", {
             id:0,
             title:null,
             desc:null,
+
+            last_red:null,
 
             year:null,
             month:null,
@@ -103,6 +105,7 @@ Vue.component("board", {
                     deadline:this.deadline,
                     createtime:String(createtime),
                     edit:false,
+                    last_red:"",
                 }
                 this.id+=1
                 this.column1.push(info)
@@ -130,7 +133,6 @@ Vue.component("board", {
             if(col==2){
                 for(let i = 0; i < this.column2.length; i++){
                     if(this.column2[i].id==id){
-                        console.log(this.column2[i])
                         this.column1.push(this.column2[i])
                         this.column2.splice(i, 1)
                 }}
@@ -147,7 +149,6 @@ Vue.component("board", {
             if(col==1){
                 for(let i = 0; i < this.column1.length; i++){
                     if(this.column1[i].id==id){
-                        console.log(this.column1[i])
                         this.column2.push(this.column1[i])
                         this.column1.splice(i, 1)
                 }}
@@ -167,6 +168,31 @@ Vue.component("board", {
                 }}
             }
         },
+        EditCard(id,titlenew,descnew,deadlinenew){
+            for(let i = 0; i < this.column1.length; i++){
+                if(this.column1[i].id==id){
+                    this.deadlin = deadlinenew
+                    this.year = this.deadlin[0] + this.deadlin[1] + this.deadlin[2] + this.deadlin[3]
+                    this.month = this.deadlin[5] + this.deadlin[6]
+                    this.day= this.deadlin[8] + this.deadlin[9]
+                    this.deadline = []
+                    this.deadline.push({day:this.day, month:this.month, year:this.year})
+                    let last_red = new Date()
+
+                    this.column1[i].title=titlenew,
+                    this.column1[i].desc=descnew,
+                    this.column1[i].deadline=this.deadline
+                    this.column1[i].last_red=String(last_red)
+            }}
+            for(let i = 0; i < this.column2.length; i++){
+                if(this.column2[i].id==id){
+                        this.column2.splice(i, 1)
+            }}
+            for(let i = 0; i < this.column3.length; i++){
+                if(this.column3[i].id==id){
+                    this.column3.splice(i, 1)
+            }}
+        }
     },
     mounted() {
     },
@@ -179,21 +205,47 @@ Vue.component("board", {
 Vue.component("card", {
     template: `
 <div class="card">
-<p>{{this.id}}</p>
-<p>{{this.title}}</p>
-<p>{{this.desc}}</p>
-<p>{{this.deadline}}</p>
-<p>{{this.createtime}}</p>
+<h4>{{this.title}}</h4>
+<p>Описание: {{this.desc}}</p>
+<p>Дедлайн: {{this.deadline[0].day}}.{{this.deadline[0].month}}.{{this.deadline[0].year}}</p>
+<p>Дата создания:{{this.createtime}}</p>
+<p>Последнее редактирование: {{this.last_red}}</p>
+
 <div>
 <button v-on:click="deletethis">Удалить карточку</button>
-<button v-if="column==2||column==3" v-on:click="moveleft">Переместить назад</button>
+<button v-if="column==1||column==2||column==3" v-on:click="editable">Редактировать</button>
+<button v-if="column==3" v-on:click="moveleft">Переместить назад</button>
 <button v-if="column==1||column==2||column==3" v-on:click="moveright">Переместить дальше</button>
+<form v-if="edit">
+<div id="redact">
 
+<div>
+    <p>Название карточки:</p>
+    <input type="text" placeholder="Название" v-model="titlenew" maxlength="15">
+</div>
+
+<div>
+<p>Описание задачи:</p>
+<input type="text" placeholder="Описание" v-model="descnew" maxlength="60">
+</div>
+
+<div>
+<input type="date" id="start" name="start" v-model="deadlinenew"
+
+min="2023-01-01" max="2030-12-31">
+<input type="submit" @click.prevent="RedCard" value="Создать карточку"> 
+</div>
+</div>
+</form>
 </div>
 </div>
     `,
     data() {
         return{
+            edit:false,
+            titlenew:null,
+            descnew:null,
+            deadlinenew:null,
         }
     },
     methods: {
@@ -206,6 +258,15 @@ Vue.component("card", {
         moveright(){
             this.$emit("moveright",this.id,this.column);
         },
+        editable(){
+            this.edit=!this.edit
+        },
+        RedCard(){
+            if(this.titlenew&&this.descnew&&this.deadlinenew){
+                this.edit=false               
+                this.$emit("edit",this.id,this.titlenew,this.descnew,this.deadlinenew); 
+            }  
+        }
     },
     mounted() {
     },
@@ -227,7 +288,10 @@ Vue.component("card", {
         },
         createtime:{
             type:String
-        }
+        },
+        last_red:{
+            type:String,
+        },
         
     },
     computed: {
